3d85892d2f50148d0b8e34f6f06a37feb00565af
nuryokusan <nuryokusan@gmail.com>
2018-06-26 04:25:14 +0200

    warmup addVectors

--- python/warmup.py
+++ python/warmup.py
@@ -1,26 +1,16 @@
 # Add your Python code here. E.g.
 from microbit import *
 
-gluecksZahl = 7
+point = [2, 2]
+direction = [1, 1]
 
-def plusEins(n):
-    return n + 1
-
-def minusEins(n):
-    return n - 1
-
-def plusGlueckszahl(n):
-    global gluecksZahl
-    return n + gluecksZahl
-
-def malZweiPlusX(n, x):
-    return n * 2 + x
-
-def malZweiPlusXMinusY(n, x, y):
-    return n * 2 + x - y
+def addVectors(v1, v2):
+    x = v1[0] + v2[0]
+    y = v1[1] + v2[1]
+    return [x, y]
 
 def check():
-    return plusGlueckszahl(1) == 8
+    return addVectors(point, direction) == [3, 3]
 
 # hier geht's los... do we smile ? :) 
 if check():
